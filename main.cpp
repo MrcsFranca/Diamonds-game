1837453a1176ac2843ce333e05ab70bcbd4614ed
MrcsFranca <francamarcos035@gmail.com>
2024-07-31 10:56:23 -0300

    improving array, adding game features and colors

--- main.cpp
+++ main.cpp
@@ -4,83 +4,155 @@
 #include <cmath>
 
 #define matriz 7
+#define diamantes 7
 #define dicas 2
 #define obstaculos1 3
 #define obstaculos2 2
-#define diamantes 7
 
 using namespace std;
 
 int main() {
-	int i, j, number_players, rounds; 
+	int i, j, k, number_players, rounds, random, linha, coluna; 
 	char coordenadas[14] = {'0','A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6'};
-	string A[matriz][matriz], move;
+	string Board[matriz][matriz], player_board[matriz][matriz], moves[36], move;
+
 
 	srand(time(NULL));
 
-	cout << "quantos jogadores? (2, 3 ou 4) ";
-	cin >> number_players;
-	int players[number_players] = {0};
+	// criação dos jogadores
+	cout << "Quantos jogadores? (2, 3 ou 4) ";
+	while (cin >> number_players && number_players != 2 && number_players != 3 && number_players != 4) {
+		cout << "\033[3;41;30mnúmero de jogadores inválido! \033[0m" << endl;
+	}
+	int players_score[number_players] = {0};
+	string players[number_players];
+
+	for (i = 0; i < number_players; i++) {
+		cin >> players[i];
+	}
+	for (i = 0; i < number_players; i++) {
+		cout << players[i] << endl;
+	}
 	
 	rounds = (pow((matriz - 1), 2));
+	
 
-
-	// criação da matriz
+	// criação da matriz do jogo
 	for (i = 0; i < matriz; i++) {
 		for (j = 0; j < matriz; j++) {
 			if (i == 0) {
-				A[i][j] = coordenadas[j];
+				Board[i][j] = coordenadas[j];
 			} else if (j == 0) {
-				A[i][j] = coordenadas[i + 6];
+				Board[i][j] = coordenadas[i + 6];
 			} else {
-				A[i][j] = A[0][j] + A[i][0];
+				Board[i][j] = "--";
+			}
+		}
+	}
+	
+	// criação da matriz do jogador
+	for (i = 0; i < matriz; i++) {
+        for (j = 0; j < matriz; j++) {
+	    	if (i == 0) {
+				player_board[i][j] = coordenadas[j];
+			} else if (j == 0) {
+				player_board[i][j] = coordenadas[i + 6];
+			} else {
+				player_board[i][j] = player_board[0][j] + player_board[i][0];
 			}
 		}
 	}
 
 
 	// criação dos diamantes
-	for (i = 0; i < diamantes; i++) {
-		A[(rand()%6) + 1][(rand()%6) + 1] = "D";
+	i = 0;
+	while (i < diamantes) {
+	    linha = (rand()%6) + 1;
+	    coluna = (rand()%6) + 1;
+	    if (Board[linha][coluna] != "D") {
+    		Board[linha][coluna] = "D";
+    		i++;
+	    }
+	}
+	
+    // criação de dicas
+    i = 0;
+    while (i < dicas) {
+	    linha = (rand()%6) + 1;
+	    coluna = (rand()%6) + 1;
+	    if (Board[linha][coluna] != "D" && Board[linha][coluna] != "O") {
+	 	    Board[linha][coluna] = "O";
+	 	    i++;
+	    }
+    }
+
+	// criação do primeiro tipo de obstaculo
+	i = 0;
+	while (i < obstaculos1) {
+	    linha = (rand()%6) + 1;
+	    coluna = (rand()%6) + 1;
+	    if (Board[linha][coluna] != "D" && Board[linha][coluna] != "O" && Board[linha][coluna] != "A") {
+	        Board[linha][coluna] = "A";
+	        i++;
+	    }
 	}
 
-	// // criação de dicas
-	// for (i = 0; i < dicas; i++) {
-	// 	A[(rand()%6) + 1][(rand()%6) + 1] = 'O';
-	// }
-
-	// // criação do primeiro tipo de obstaculo
-	// for (i = 0; i < obstaculos1; i++) {
-	// 	A[(rand()%6) + 1][(rand()%6) + 1] = 'A';
-	// }
-
 	// // criação do segundo tipo de obstaculo
-	// for (i = 0; i < obstaculos1; i++) {
-	// 	A[(rand()%6) + 1][(rand()%6) + 1] = 'a';
-	// }
+	i = 0;
+	while (i < obstaculos2) {
+	    linha = (rand()%6) + 1;
+	    coluna = (rand()%6) + 1;
+	    if (Board[linha][coluna] != "D" && Board[linha][coluna] != "O" && Board[linha][coluna] != "A" && Board[linha][coluna] != "a") {
+	        Board[linha][coluna] = "a";
+	        i++;
+	    }
+	}
 
 
 	// jogo
 	while (rounds--) {
 		//imprimir placar
-		cout << "----- PLACAR -----" << endl;
+		cout << "\033[3;43;30m----- PLACAR -----\033[0m" << endl;
 		for (i = 0; i < number_players; i++) {
-			cout << "Jogador" << i + 1 << ": " << players[i] << endl;
+			cout << "\x1B[3" << i + 1 << "m" << players[i] << ":\033[0m " << players_score[i] << endl;
 		}
-		cout << "Faltam " << rounds + 1 << " para acabar o jogo" << endl;
+		cout << "Faltam " << rounds + 1 << " rodadas para acabar o jogo" << endl;
 
-		// imprimir matriz
-		cout << "----- TABULEIRO -----" << endl;
+		// imprimir tabuleiro
+		cout << "\033[3;43;30m----- TABULEIRO -----\033[0m" << endl;
 		for (i = 0; i < matriz; i++) {
 			for (j = 0;  j < matriz; j++) {
-				cout << setw(9) << A[i][j];
+				cout << setw(9) << Board[i][j];
 			}
 			cout << endl;
 		}
 
-		for (i = 0; i < number_players; i++) {
-			cout << "Ação do jogador" << i + 1 << ": ";
+		// imprimir tabuleiro do jogador
+		cout << endl << endl;
+		for (i = 0; i < matriz; i++) {
+			for (j = 0;  j < matriz; j++) {
+				cout << setw(9) << player_board[i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;
+		
+
+		// Ação do jogador
+		for (k = 0; k < number_players; k++) {
+			cout << "\x1B[3" << k + 1 << "m" << "Ação do jogador " << players[k] << ":\033[0m ";
 			cin >> move;
+			for (i = 0; i < matriz; i++) {
+			    for (j = 0; j < matriz; j++) {
+			        if (move == player_board[i][j]) {
+						moves[rounds - 1] = move;
+			            player_board[i][j] = Board[i][j];
+						if (Board[i][j] == "D") {
+							players_score[k]++;
+						}
+			        }
+			    }
+			}
 			cout << endl;
 		}
 	}
